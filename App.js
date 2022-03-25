dead722f1cdfc55bdb0d402234061c74ec17f055
olivier bouillet <olivier.bouillet@ifeelsmart.com>
2022-03-25 22:48:04 +0100

    chore: update sample

--- App.js
+++ App.js
@@ -1,134 +1,33 @@
 import React, {useRef} from 'react';
-import {
-  View,
-  Text,
-  useTVEventHandler,
-  TouchableOpacity,
-  TextInput,
-  Pressable,
-  Image,
-  ScrollView,
-  Dimensions,
-  FlatList,
-  LogBox,
-  Platform,
-} from 'react-native';
-import faker from 'faker';
-
-const {width, height} = Dimensions.get('screen');
+import {View, TextInput, LogBox} from 'react-native';
 
 const TVEventHandlerView = () => {
-  const [lastEventType, setLastEventType] = React.useState('');
-  const [ActiveIndex, setActiveIndex] = React.useState(0);
-  const NumberofColumn = 5;
+  const [show, setShow] = React.useState(true);
 
   const firstRef = useRef();
-  const secondRef = useRef();
-
-  faker.seed(20);
-  const DATA = [...Array(150).keys()].map((_, i) => {
-    return {
-      image: `https://randomuser.me/api/portraits/${faker.helpers.randomize([
-        'women',
-        'men',
-      ])}/${faker.random.number(60)}.jpg`,
-      name: faker.name.findName(),
-    };
-  });
-
-  const myTVEventHandler = (evt) => {
-    useTVEventHandler(myTVEventHandler);
-
-    if (evt.eventType !== 'blur' && evt.eventType !== 'focus') {
-      setLastEventType(evt.eventType);
-    }
-  };
-
-  const Shadow = () => {
-    return {
-      shadowColor: '#000',
-      shadowOffset: {width: 0, height: 20},
-      shadowOpacity: 0.2,
-      shadowRadius: 2,
-      elevation: 5,
-      zIndex: 50,
-    };
-  };
-
-  /*
-      <FlatList
-        data={DATA}
-        numColumns={NumberofColumn}
-        renderItem={({item, index}) => (
-          <TouchableOpacity
-            key={index}
-            onFocus={() => setActiveIndex(index)}
-            activeOpacity={1}
-            onPress={() => alert(item.name)}>
-            <View
-              style={[
-                {
-                  justifyContent: 'center',
-                  flex: 1,
-                  alignItems: 'center',
-                  height: 200,
-                  margin: 5,
-                },
-                ActiveIndex == index && Shadow(),
-              ]}>
-              <Image
-                source={{uri: item.image}}
-                style={{height: 200, width: '100%'}}
-              />
-            </View>
-            <View
-              style={{
-                backgroundColor: 'red',
-                marginHorizontal: 5,
-                marginVertical: 15,
-                width: width / NumberofColumn,
-              }}>
-              {ActiveIndex == index && (
-                <Text numberOfLines={1} ellipsizeMode="tail">
-                  {item.name}
-                </Text>
-              )}
-            </View>
-          </TouchableOpacity>
-
-        )}
-      />
-
-  */
 
   return (
-    <View>
-      <TextInput
-        ref={firstRef}
-        defaultValue={'hello'}
-        showSoftInputOnFocus={true}
-        autoFocus={true}
-        onSubmitEditing={() => {}}
-      />
-      <TouchableOpacity
-        ref={secondRef}
-        //onFocus={() => setActiveIndex(index)}
-        activeOpacity={1}
-        onPress={() => alert('Hello!')}>
-        <View
-          style={[
-            {
-              justifyContent: 'center',
-              flex: 1,
-              alignItems: 'center',
-              height: 200,
-              margin: 5,
-            },
-          ]}>
-          <Text>blabla</Text>
-        </View>
-      </TouchableOpacity>
-    </View>
+    <>
+      {show && (
+        <>
+          <TextInput
+            ref={firstRef}
+            defaultValue={'hello'}
+            showSoftInputOnFocus={true}
+            autoFocus={true}
+            editable={true}
+            onChangeText={() => {
+              console.log('onTextChange');
+            }}
+            onSubmitEditing={() => {
+              console.log('onSubmitEditing');
+              setShow(false);
+            }}
+            placeholder={'Search...'}
+          />
+        </>
+      )}
+    </>
   );
 };
 
