05810a1daa8adaf3479b586fba133e1bb81fccc3
olivier bouillet <olivier.bouillet@ifeelsmart.com>
2022-03-25 17:30:10 +0100

    chore: update for testing input text

--- App.js
+++ App.js
@@ -1,25 +1,45 @@
-import React from 'react'
-import { View, Text, useTVEventHandler, TouchableOpacity, Pressable, Image, ScrollView, Dimensions, FlatList, LogBox, Platform } from 'react-native'
-import faker from 'faker'
+import React, {useRef} from 'react';
+import {
+  View,
+  Text,
+  useTVEventHandler,
+  TouchableOpacity,
+  TextInput,
+  Pressable,
+  Image,
+  ScrollView,
+  Dimensions,
+  FlatList,
+  LogBox,
+  Platform,
+} from 'react-native';
+import faker from 'faker';
 
-const { width, height } = Dimensions.get('screen')
+const {width, height} = Dimensions.get('screen');
 
 const TVEventHandlerView = () => {
   const [lastEventType, setLastEventType] = React.useState('');
   const [ActiveIndex, setActiveIndex] = React.useState(0);
-  const NumberofColumn = 5
+  const NumberofColumn = 5;
+
+  const firstRef = useRef();
+  const secondRef = useRef();
 
   faker.seed(20);
   const DATA = [...Array(150).keys()].map((_, i) => {
     return {
-      image: `https://randomuser.me/api/portraits/${faker.helpers.randomize(['women', 'men'])}/${faker.random.number(60)}.jpg`,
+      image: `https://randomuser.me/api/portraits/${faker.helpers.randomize([
+        'women',
+        'men',
+      ])}/${faker.random.number(60)}.jpg`,
       name: faker.name.findName(),
     };
   });
 
-  const myTVEventHandler = evt => {
+  const myTVEventHandler = (evt) => {
     useTVEventHandler(myTVEventHandler);
-    if (evt.eventType != 'blur' && evt.eventType != 'focus') {
+
+    if (evt.eventType !== 'blur' && evt.eventType !== 'focus') {
       setLastEventType(evt.eventType);
     }
   };
@@ -27,47 +47,96 @@ const TVEventHandlerView = () => {
   const Shadow = () => {
     return {
       shadowColor: '#000',
-      shadowOffset: { width: 0, height: 20 },
+      shadowOffset: {width: 0, height: 20},
       shadowOpacity: 0.2,
       shadowRadius: 2,
       elevation: 5,
-      zIndex: 50
-    }
-  }
+      zIndex: 50,
+    };
+  };
+
+  /*
+      <FlatList
+        data={DATA}
+        numColumns={NumberofColumn}
+        renderItem={({item, index}) => (
+          <TouchableOpacity
+            key={index}
+            onFocus={() => setActiveIndex(index)}
+            activeOpacity={1}
+            onPress={() => alert(item.name)}>
+            <View
+              style={[
+                {
+                  justifyContent: 'center',
+                  flex: 1,
+                  alignItems: 'center',
+                  height: 200,
+                  margin: 5,
+                },
+                ActiveIndex == index && Shadow(),
+              ]}>
+              <Image
+                source={{uri: item.image}}
+                style={{height: 200, width: '100%'}}
+              />
+            </View>
+            <View
+              style={{
+                backgroundColor: 'red',
+                marginHorizontal: 5,
+                marginVertical: 15,
+                width: width / NumberofColumn,
+              }}>
+              {ActiveIndex == index && (
+                <Text numberOfLines={1} ellipsizeMode="tail">
+                  {item.name}
+                </Text>
+              )}
+            </View>
+          </TouchableOpacity>
+
+        )}
+      />
+
+  */
 
   return (
-    <FlatList
-      data={DATA}
-      numColumns={NumberofColumn}
-      renderItem={({ item, index }) =>
-        <TouchableOpacity key={index} onFocus={() => setActiveIndex(index)} activeOpacity={1} onPress={() => alert(item.name)}>
-          <View style={[{
-            justifyContent: 'center',
-            flex: 1,
-            alignItems: 'center',
-            height: 200,
-            margin: 5,
-          }, ActiveIndex == index && Shadow()]}>
-            <Image source={{ uri: item.image }} style={{ height: 200, width: '100%' }} />
-          </View>
-          <View style={{ backgroundColor: 'red', marginHorizontal: 5, marginVertical: 15, width: width / NumberofColumn, }}>
-            {ActiveIndex == index &&
-              <Text numberOfLines={1} ellipsizeMode='tail'>{item.name}</Text>
-            }
-          </View>
-        </TouchableOpacity>
-      }
-    />
+    <View>
+      <TextInput
+        ref={firstRef}
+        defaultValue={'hello'}
+        showSoftInputOnFocus={true}
+        autoFocus={true}
+        onSubmitEditing={() => {}}
+      />
+      <TouchableOpacity
+        ref={secondRef}
+        //onFocus={() => setActiveIndex(index)}
+        activeOpacity={1}
+        onPress={() => alert('Hello!')}>
+        <View
+          style={[
+            {
+              justifyContent: 'center',
+              flex: 1,
+              alignItems: 'center',
+              height: 200,
+              margin: 5,
+            },
+          ]}>
+          <Text>blabla</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
   );
 };
 
-
 export default function App() {
-  LogBox.ignoreAllLogs()
+  LogBox.ignoreAllLogs();
   return (
-    <View style={{ flex: 1 }}>
+    <View style={{flex: 1}}>
       <TVEventHandlerView />
     </View>
-  )
+  );
 }
-
